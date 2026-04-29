a2f703fe0ca314c4211055d83ad88bccc066b418
Andrés Alcaraz Rey <nomail@nomail.com>
2026-04-29 19:35:06 +0200

    Cliente importación añadido

---
+++ importDataClient/importDataClient/src/main/java/com/politecnicomalaga/Main.java
@@ -0,0 +1,9 @@
+package com.politecnicomalaga;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Crea el código necesario para abrir el fichero CSV y mandar los datos al server!");
+
+    }
+}
