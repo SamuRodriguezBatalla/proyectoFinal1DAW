0c1f9c8cd9f27763c9078a33460ddd9a23852e0e
Andrés Alcaraz Rey <nomail@nomail.com>
2026-04-29 21:02:06 +0200

    examen simplificado añadido

--- servlets/src/main/java/com/politecnicomalaga/tienda/controller/DataAccess.java
+++ servlets/src/main/java/com/politecnicomalaga/tienda/controller/DataAccess.java
@@ -5,7 +5,5 @@ public interface DataAccess {
     public String findProductoXCodigo(String code);
     public String findClienteXDNI(String dni);
     public String listProductosXPedido(String dni, String pedido);
-    public String insertProducto(String jsonProducto);
-    public String insertCliente(String jsonCliente);
-    public String insertPedido(String jsonPedido);
+    public String importData(String jsonDataFromCSV);
 }
